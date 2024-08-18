d5d43e29a32388fa478160ac2f63e3fbd4cdc039
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2024-08-18 23:44:03 +0530

    fixed mail service

--- packages/mailer/src/index.ts
+++ packages/mailer/src/index.ts
@@ -1,21 +1,37 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
-const transport = nodemailer.createTransport({
-  secure: true,
-  host: "smtp.gmail.com",
-  port: 465,
-  auth: {
-    user: process.env.SENDER_EMAIL,
-    pass: process.env.SENDER_PASSWORD,
-  },
-});
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 export async function sendEmail(to: string, body: string) {
-  await transport.sendMail({
-    from: process.env.SENDER_EMAIL,
-    sender: process.env.SENDER_EMAIL,
-    to,
-    subject: "Hello from Yapier!",
-    text: body,
-  });
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.SENDER_EMAIL,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken as string,
+      },
+    });
+    await transport.sendMail({
+      from: "Yapier",
+      sender: process.env.SENDER_EMAIL,
+      to,
+      subject: "Hello from Yapier!",
+      text: body,
+      html: `<p>${body}</p>`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
